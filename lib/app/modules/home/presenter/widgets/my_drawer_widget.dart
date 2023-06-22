5a2a27e66f43ff8e05a0c6addb8486b54a196ebc
Lucas Silva <lucassilva080397@gmail.com>
2023-06-22 17:46:25 -0300

    new version

--- lib/app/modules/home/presenter/widgets/my_drawer_widget.dart
+++ lib/app/modules/home/presenter/widgets/my_drawer_widget.dart
@@ -118,7 +118,7 @@ class _MyDrawerWidgetState extends State<MyDrawerWidget> {
                     ),
                     const ListTile(
                       title: Text(
-                        'Versão 30.0.0',
+                        'Versão 31.0.0',
                         textAlign: TextAlign.end,
                       ),
                     ),
